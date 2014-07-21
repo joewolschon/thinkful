2439c96f9edebe21a8a0b740bd984be18b5123a9
Joseph Wolschon <jwolschon@payjunction.com>
2014-07-21 11:50:06 -0700

    lesson 1 assignment 2

---
+++ assets/calculator.js
@@ -0,0 +1,28 @@
+var errorText = "X and Y must be integers";
+
+function add(x, y)
+{
+    if (!isNaN(x) && !isNaN(y))
+    {
+        setResult(x + " plus " + y + " is " + (x + y));
+    } else
+    {
+        setResult(errorText);
+    }
+}
+
+function subtract(x, y)
+{
+    if (!isNaN(x) && !isNaN(y))
+    {
+        setResult(x + " minus " + y + " is " + (x - y));
+    } else
+    {
+        setResult(errorText)
+    }
+}
+
+function setResult(result)
+{
+    document.getElementById('result').innerHTML = result;
+}
