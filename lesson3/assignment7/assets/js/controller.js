c2503d85c05b5a4b04ed18b0b3a40111ab7a9cc1
eevne <jwolschon@payjunction.com>
2014-08-11 21:30:33 -0700

    code refactor

--- lesson3/assignment7/assets/js/controller.js
+++ lesson3/assignment7/assets/js/controller.js
@@ -1,42 +1,37 @@
 angular.module('app', []).controller('CalculatorCtrl', function ($scope) {
 
-  $scope.submit = function () {
-    console.log("submit");
-    if (!isNaN($scope.mealDetails.taxRate) && !isNaN($scope.mealDetails.taxRate) && !isNaN($scope.mealDetails.tipPercentage)) {
-      var base = parseFloat($scope.mealDetails.baseMealPrice);
-      $scope.totals.subTotal = base + (base * (parseFloat($scope.mealDetails.taxRate) / 100));
-      $scope.totals.tip = base * (parseFloat($scope.mealDetails.tipPercentage) / 100);
+  $scope.totalDefaults = {
+   subTotal: 0.00,
+   tip: 0.00,
+   total: 0.00,
+   tipTotal: 0.00,
+   mealCount: 0,
+   averageTipPerMeal: 0.00};
+
+  $scope.totals = angular.copy($scope.totalDefaults);
+  $scope.details = {};
+
+  $scope.submit = function (details) {
+    if($scope.form.$valid){
+      var base = parseFloat(details.baseMealPrice);
+      $scope.totals.subTotal = base + (base * (parseFloat(details.taxRate) / 100));
+      $scope.totals.tip = base * (parseFloat(details.tipPercentage) / 100);
       $scope.totals.total = $scope.totals.subTotal + $scope.totals.tip;
 
       $scope.totals.tipTotal = $scope.totals.tipTotal + $scope.totals.tip;
       $scope.totals.mealCount = $scope.totals.mealCount + 1;
       $scope.totals.averageTipPerMeal = $scope.totals.tipTotal / $scope.totals.mealCount;
-
-      $scope.clearMealDetails();
-    } else {
-      $scope.mealDetails.error = 'Invalid Input';
     }
   };
 
-  $scope.clearMealDetails = function () {
-    $scope.mealDetails = {
-      baseMealPrice: NaN,
-      taxRate: NaN,
-      tipPercentage: NaN,
-      errorMessage: ''
-    };
+  $scope.clearDetails = function () {
+    $scope.details = {};
+    $scope.form.$setPristine();
   };
 
   $scope.reset = function () {
-    $scope.clearMealDetails();
-    $scope.totals = {
-      subTotal: 0.00,
-      tip: 0.00,
-      total: 0.00,
-      tipTotal: 0.00,
-      mealCount: 0,
-      averageTipPerMeal: 0.00
-    };
+    $scope.clearDetails();
+    angular.copy($scope.totalDefaults, $scope.totals);
   };
 
 });
