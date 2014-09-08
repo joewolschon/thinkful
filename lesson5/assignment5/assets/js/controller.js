53d8ca05e5de10ac94c4eb1cb9ab09eef8c861a3
Joseph Wolschon <jwolschon@payjunction.com>
2014-09-08 14:54:52 -0700

    lesson 5 assignment 5

--- lesson5/assignment5/assets/js/controller.js
+++ lesson5/assignment5/assets/js/controller.js
@@ -1,24 +1,23 @@
-angular.module('app', []).controller('InstagramSearcherCtrl', function ($scope, $http, $q) {
+angular.module('app', []).controller('InstagramSearcherCtrl', function ($scope, $http, $q, $log) {
+
+  $scope.search = '';
 
   $scope.submit = function () {
-    getAcessToken();
-  };
-
-  function getAcessToken() {
     $http({
-      url: '[https://api.instagram.com/v1/tags/search',
+      url: 'https://api.instagram.com/v1/tags/search',
+      method: 'GET',
       params: {
         client_id: '749d8265ce234c198ed79664225b0660',
         client_secret: 'fb3967fbb9764bd9b6d112b9d9bcaa49',
         grant_type: 'authorization_code',
         redirect_uri: 'http://eevne.github.io/thinkful/',
-        q: 'dog'
+        q: $scope.search
       }
     }).success(function (data) {
-      console.log(data);
+      $log.log('success', data);
     }).error(function (data) {
-
+      $log.log('error', data);
     });
+  };
 
-  }
 });
