25dd34ccc744b84adbb125306cd0857e0f418c27
eevne <jwolschon@payjunction.com>
2014-09-21 12:42:01 -0700

    animated waitstaff and open weather map

--- unit2/lesson1/assignment3/app.js
+++ unit2/lesson1/assignment3/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('waitstaff', ['ngRoute']);
+var app = angular.module('waitstaff', ['ngRoute', 'ngAnimate']);
 
 app.config(function ($routeProvider) {
 
--- unit2/lesson1/open-weather-map/app/owm-app.js
+++ unit2/lesson1/open-weather-map/app/owm-app.js
@@ -1,37 +1,49 @@
-var app = angular.module('OWMApp', ['ngRoute']);
+var app = angular.module('OWMApp', ['ngRoute', 'ngAnimate']);
 app.value('owmCities', ['New York', 'Dallas', 'Chicago']);
+
 app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/',
-        {
-            templateUrl: './home.html',
-            controller: 'HomeCtrl'})
-        .when('/error',
-        {
-            template: '<p>Error Page Not Found</p>'})
-        .when('/cities/:city',
-        {
-            templateUrl: './city.html',
-            controller: 'CityCtrl',
-            resolve: {
-                city: function (owmCities, $route, $location) {
-                    var city = $route.current.params.city;
-                    if (owmCities.indexOf(city) == -1) {
-                        $location.path('/error');
-                        return;
-                    }
-                    return city;
+    $routeProvider.when('/', {templateUrl: './home.html', controller: 'HomeCtrl'});
+
+    $routeProvider.when('/error', {
+        template: '<p>Error Page Not Found</p>'
+    });
+
+    $routeProvider.when('/cities/:city', {
+        templateUrl: './city.html',
+        controller: 'CityCtrl',
+        resolve: {
+            city: function (owmCities, $route, $location) {
+                var city = $route.current.params.city;
+                if (owmCities.indexOf(city) == -1) {
+                    $location.path('/error');
+                    return;
                 }
+                return city;
             }
-        })
-        .otherwise({
-            redirectTo: '/error'
-        });
+        }
+    });
+
+    $routeProvider.otherwise({redirectTo: '/error'});
+});
+
+app.run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$routeChangeError', function() {
+        $location.path("/error");
+    });
+    $rootScope.$on('$routeChangeStart', function() {
+        $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+        $timeout(function() {
+            $rootScope.isLoading = false;
+        }, 1000);
+    });
 });
 
 app.controller('HomeCtrl', function ($scope) {
     //empty for now
 });
+
 app.controller('CityCtrl', function ($scope, city) {
     $scope.city = city;
 });
