be15330a3c79f4c6b8b156d7b7e3f66cc6e036c0
Joseph Wolschon <jwolschon@payjunction.com>
2014-10-22 15:16:35 -0700

    unit 3 lesson 1 assignment 4 - moved some files around

---
+++ unit3/lesson1/assignment4/gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp'),
+gutil = connect = require('gulp-util'),
+connect = require('gulp-connect'),
+concat = require('gulp-concat'),
+sourcemaps = require('gulp-sourcemaps'),
+plumber = require('gulp-plumber'),
+uglify = require('gulp-uglify'),
+livereload = require('gulp-livereload');
+
+var paths = {
+  js: [
+    '!*.js', 'app/**/*.js', '!**/*.min.js', '!app/bower_components/**/*.js'
+  ],
+  css: [
+    'app/**/*.css', '!**/*.min.css'
+  ]
+};
+
+gulp.task('start-livereload', function() {
+  livereload.listen();
+});
+
+gulp.task('package', ['js', 'css']);
+
+gulp.task('dev', ['start-livereload', 'package'], function() {
+  gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.css, ['css']);
+
+//  gulp.watch(paths.ftl.all).on('change', livereload.changed);
+});
+
+gulp.task('css', function(){
+  function createCssPackage (cssPaths, destFile, destDir) {
+    gulp.src(cssPaths)
+      .pipe(plumber(function(err) {
+        gutil.log(gutil.colors.red(err));
+        gutil.beep();
+      }))
+      .pipe(concat(destFile))
+      .pipe(gulp.dest(destDir))
+  }
+
+  createCssPackage(paths.css, 'combined.min.css', 'app/assets/css/');
+});
+
+gulp.task('js', function(){
+  function createJsPackage (jsPaths, destFile, destDir, sourceRoot) {
+    gulp.src(jsPaths)
+      .pipe(plumber(function(err) {
+        gutil.log(gutil.colors.red(err));
+        gutil.beep();
+      }))
+      .pipe(sourcemaps.init())
+      .pipe(concat(destFile))
+      .pipe(uglify({mangle: false}))
+      .pipe(sourcemaps.write('.', {sourceRoot: sourceRoot}))
+      .pipe(gulp.dest(destDir))
+  }
+
+  createJsPackage(paths.js, 'combined.min.js', 'app/assets/js/', '.');
+});
+
+gulp.task('connect', function () {
+  connect.server({
+    root: 'app/'
+  });
+});
+
+gulp.task('default', ['dev', 'js', 'css', 'connect']);
