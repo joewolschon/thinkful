c8cdb3bcf87a2674cbcb20f2f42e24eaeb0a73b4
Joseph Wolschon <jwolschon@payjunction.com>
2014-10-22 12:49:11 -0700

    unit 3 lesson 1 assignment 2 - moved some files around

--- unit3/lesson1/assignment2/app/assets/opt-in/assets/js/directives/optIn.js
+++ unit3/lesson1/assignment2/app/assets/opt-in/assets/js/directives/optIn.js
@@ -2,7 +2,7 @@ angular.module('app').directive('optIn', function(){
    return {
      restrict: 'E',
      transclude: true,
-     templateUrl: '/opt-in/assets/partials/opt-in.html',
+     templateUrl: 'assets/opt-in/assets/partials/opt-in.html',
      replace: true
    }
 });
